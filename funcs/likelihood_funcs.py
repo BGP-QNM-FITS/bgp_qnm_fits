tidying + fixed scaling

funcs/likelihood_funcs.py
@@ -156,28 +156,25 @@ def get_element(array1, array2, analysis_times, covariance_matrix):
     """
     This computes a single element of the Fisher matrix corresponding to dict1 and dict2.
 
+    The element is given by the inner product of the two arrays, weighted by the covariance 
+    matrix. In general this converges to an integral in the limit of a large number of
+    analysis times. However, for the case of a diagonal covariance matrix (i.e. white noise), the
+    inner product must be scaled by dt. 
+
     Returns a real scalar.
+    
     """
 
-    #return np.real(
-    #    np.einsum("bi,bj,bij->", array1, array2, covariance_matrix)
-    #    / (analysis_times[-1] - analysis_times[0]) 
-    #)
-
-    #if np.allclose(covariance_matrix[0], np.diag(np.diagonal(covariance_matrix[0]))): # should really make this true for every mode
-    #    return np.real(
-    #        np.einsum("bi,bj,bij->", array1, array2, covariance_matrix)
-    #        * (analysis_times[-1] - analysis_times[0]) / len(analysis_times)
-    #    )
-    #else:
-    #    return np.real(
-    #        np.einsum("bi,bj,bij->", array1, array2, covariance_matrix)
-    #    )
- 
-    return np.real(
-        np.einsum("bi,bj,bij->", array1, array2, covariance_matrix)
-        * (analysis_times[-1] - analysis_times[0]) / len(analysis_times)
-    )
+    # TODO: Make this more general
+    if np.allclose(covariance_matrix[0], np.diag(np.diagonal(covariance_matrix[0]))):
+        return np.real(
+            np.einsum("bi,bj,bij->", array1, array2, covariance_matrix)
+            * (analysis_times[-1] - analysis_times[0]) / len(analysis_times)
+        )
+    else:
+        return np.real(
+            np.einsum("bi,bj,bij->", array1, array2, covariance_matrix)
+        )
 
 
 def get_fisher_matrix(

funcs/qnm_selecting_funcs.py
@@ -56,19 +56,22 @@ def get_significance(marginal_mean, marginal_covariance):
     """
 
     # TODO: Fine tune b_a threshold for significance
-    # TODO: Deal with stability problems 
+    # TODO: Stability problems arise with n=7 everywhere. Can probably remove regularization here. 
+    # Main thing to resolve is generic instability caused by n=7. 
 
     covariance = marginal_covariance.copy() 
 
-    try:
-        L = cholesky(covariance)
-    except np.linalg.LinAlgError: 
-        covariance += np.eye(covariance.shape[0]) * 1e-10
+    regularization = 1e-15
+    while True:
+        covariance += np.eye(covariance.shape[0]) * regularization
         try:
             L = cholesky(covariance, lower=True)
+            break
         except np.linalg.LinAlgError:
-            print("Covariance matrix is not positive definite.")
-            return np.nan
+            regularization *= 10
+            if regularization > 1e-9:
+                print("Covariance matrix could not be regularized to positive definite.")
+                return np.nan
 
     b_a = -np.dot(np.linalg.inv(L), marginal_mean)
 
@@ -232,7 +235,7 @@ def get_qnm_timeseries(
 
         qnm_list_timeseries.append(qnm_list_new)
 
-        if qnm_order_reduced == [] or qnm_list_new == []:
+        if qnm_order_reduced == [] or qnm_list_new == []: # TODO: This can actually break when there's only one element 
             break 
 
     return qnm_list_timeseries

get_kernel_params.py
@@ -6,50 +6,9 @@ from funcs.utils import *
 from funcs.kernel_param_funcs import *
 import pickle
 
-SIMNUMS = [
-    "0001",
-    "0002",
-    "0003",
-    "0004",
-    "0005",
-    "0006",
-    "0007",
-    "0008",
-    "0009",
-    "0010",
-    "0011",
-    "0012",
-    "0013",
-]
-
-RINGDOWN_START_TIMES = [
-    17.0,
-    21.0,
-    23.0,
-    26.0,
-    17.0,
-    17.0,
-    17.0,
-    11.0,
-    29.0,
-    16.0,
-    12.0,
-    17.0,
-    6.0,
-]
-
-TRAINING_SPH_MODES = [
-    (2, 2),
-    (2, 1),
-    (3, 3),
-    (3, 2),
-    (4, 4),
-    (2, -2),
-    (2, -1),
-    (3, -3),
-    (3, -2),
-    (4, -4),
-]
+SIMNUMS = ["0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011", "0012", "0013"]
+RINGDOWN_START_TIMES = [17.0, 21.0, 23.0, 26.0, 17.0, 17.0, 17.0, 11.0, 29.0, 16.0, 12.0, 17.0, 6.0]
+TRAINING_SPH_MODES = [(2, 2), (2, 1), (3, 3), (3, 2), (4, 4), (2, -2), (2, -1), (3, -3), (3, -2), (4, -4)]
 
 SMOOTHNESS = 16
 EPSILON = 1 / 10
@@ -58,7 +17,7 @@ EPSILON = 1 / 10
 
 TRAINING_START_TIME = -10
 TRAINING_END_TIME = 100
-TIME_STEP = 0.5
+TIME_STEP = 0.1
 
 # These are the bounds of the minimisation for the kernel hyperparameters
 
@@ -104,6 +63,10 @@ param_dict_sim_lm = {}
 
 print("Getting parameters...")
 
+# This first section is extremely generic and can be used for any subset of modes or simulations. In principle if 
+# the timesteps in R are downsampled this might slightly alter the values of sigma_max and sigma_min
+# but this shouldn't affect things too much. 
+
 for i, sim_id in enumerate(SIMNUMS):
 
     print(sim_id)
@@ -135,41 +98,21 @@ with open("param_dict_sim_lm_full.pkl", "wb") as f:
 with open("R_dict_sim_lm_full.pkl", "wb") as f:
     pickle.dump(R_sim_lm, f)
 
-breakpoint() 
-
 print("Getting hyperparameters...")
 
-analysis_times = np.arange(
-    TRAINING_START_TIME, TRAINING_START_TIME + TRAINING_END_TIME, TIME_STEP
-)
-
-hyperparam_list, le = get_minimised_hyperparams(
+hyperparam_list, le, tuned_params_sim_lm = train_hyper_params(
+    TRAINING_START_TIME,
+    TRAINING_END_TIME,
+    TIME_STEP,
     INITIAL_PARAMS,
     BOUNDS,
     param_dict_sim_lm,
     R_sim_lm,
     HYPERPARAM_RULE_DICT,
-    analysis_times,
     kernel_main,
     TRAINING_SPH_MODES,
     SIM_TRAINING_MODE_RULES,
 )
-
-print(
-    "Optimal parameters:",
-    dict(zip(HYPERPARAM_RULE_DICT.keys(), hyperparam_list)),
-    "Log evidence:",
-    le,
-)
-
-print("Tuning parameters...")
-
-tuned_params_sim_lm = {}
-
-for sim_id in SIMNUMS:
-    tuned_params_sim_lm[sim_id] = get_tuned_params(
-        param_dict_sim_lm[sim_id], hyperparam_list, HYPERPARAM_RULE_DICT
-    )
 
 with open("tuned_params.pkl", "wb") as f:
     pickle.dump(tuned_params_sim_lm, f)